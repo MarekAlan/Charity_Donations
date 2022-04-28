45de74b309295ebe6597cdd888f76122f3c9adb9
MM <Ja@MMs-MacBook-Air.local>
2022-04-29 00:34:56 +0200

    ok

--- charity_app/models.py
+++ charity_app/models.py
@@ -1,3 +1,36 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+TYPES = (
+    ("Fundacja", "Fundacja"),
+    ("Organizacja Pozarządowa", "Organizacja Pozarządowa"),
+    ("Zbiórka Lokalna", "Zbiórka Lokalna"),
+)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.CharField(max_length=64, choices=TYPES, default="Fundacja")
+    categories = models.ManyToManyField(Category)
+
+
+class Donation(models.Model):
+    quantity = models.SmallIntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=16)
+    city = models.CharField(max_length=32)
+    zip_code = models.CharField(max_length=10)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
--- charity_app/views.py
+++ charity_app/views.py
@@ -3,11 +3,22 @@ from django.shortcuts import render
 # Create your views here.
 from django.views import View
 
+from charity_app.models import Institution, Donation
+
 
 class LandingPageView(View):
 
     def get(self, request):
-        return render(request, 'index.html')
+        nb_bags = 0
+        donations = Donation.objects.all()
+        for donation in donations:
+            donation.quantity += nb_bags
+        nb_organizations = Institution.objects.all().count()
+        ctx = {
+            "nb_bags": nb_bags,
+            "nb_organizations": nb_organizations,
+        }
+        return render(request, 'index.html', ctx)
 
 
 class AddDonationView(View):
